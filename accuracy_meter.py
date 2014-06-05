b769f290c8104180d662d0086a6f65382b1706ca
imozerov <ilyailiilia@gmail.com>
2014-06-05 20:42:45 +0400

    Added TabFileParser class

--- accuracy_meter.py
+++ accuracy_meter.py
@@ -3,7 +3,24 @@
 class AccuracyMeter(object):
     pass
 
+class TabFileParser(object):
+    def __init__(self):
+        self.file_lines = None
+        self.sentences = []
 
+    def parse_sentences(self):
+        sentence = []
+        for line in self.file_lines:
+            if line == "\n":
+                self.sentences.append(sentence)
+                sentence = []
+            else:
+                sentence.append(line)
+        self.file_lines = None
+
+    def read_lines(self, path):
+        with open(path, "r") as f:
+            self.file_lines = f.readlines()
 
 if __name__ == "__main__":
     pass
--- accuracy_meter_test.py
+++ accuracy_meter_test.py
@@ -4,18 +4,33 @@ import unittest
 
 from accuracy_meter import *
 
-class AccuracyMeterTest(unittest.TestCase):
-    expected_path = "test_data/expected.txt"
-    now_path = "test_data/now.txt"
+expected_path = "test_data/expected.txt"
+now_path = "test_data/now.txt"
 
+
+class AccuracyMeterTest(unittest.TestCase):
     def test_compareShouldReturn100PercentForExactMatch(self):
         meter = AccuracyMeter()
-        result = meter.compare(self.expected_path, self.expected_path)
+        result = meter.compare(expected_path, expected_path)
 
         self.assertEquals(1, result)
 
     def test_comparePartlyEqualFilesShouldReturnCorrectNumber(self):
         meter = AccuracyMeter()
-        result = meter.compare(self.expected_path, self.now_path)
+        result = meter.compare(expected_path, now_path)
 
         self.assertEquals(0.75, result)
+
+
+class TabFileParserTest(unittest.TestCase):
+    expected_first_sentence = ["Ведь\tPART\t7\tогранич\n", "текст\tS.m.nom.sg\t7\tпредик\n",
+                                "закона\tS.gen.m.sg\t2\tквазиагент\n", "в\tPR\t7\tобст\n", "этом\tA.m.prep.sg\t6\tопред\n",
+                                "случае\tS.m.prep.sg\t4\tпредл\n", "остается\tV.3p.real.sg\t0\tROOT\n",
+                                "прежним\tA.ins.m.sg\t7\tприсвяз\n"]
+
+    def test_getSentencesShouldParseSentencesCorrectly(self):
+        parser = TabFileParser()
+        parser.read_lines(expected_path)
+        parser.parse_sentences()
+
+        self.assertIn(self.expected_first_sentence, parser.sentences)
